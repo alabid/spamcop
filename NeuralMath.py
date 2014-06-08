872a4cfc2acc0ee6189582fa67f47c5014ffde03
schifelw <schifelw@cmc307-02.mathcs.carleton.edu>
2014-06-08 15:56:16 -0500

    removed neuralNetwork2 and added neuralmath

--- NeuralMath.py
+++ NeuralMath.py
@@ -11,17 +11,19 @@ from NeuralNetworkClassifier import *
 random.seed(0)
 
 
-
+# function given neural network and examples, predicts for each example,
+# and will tell you the average error, used for math functions.
 def test_on_new_examples(network, new_examples):
     total = len(new_examples)
     error = 0.0
     for example in new_examples:
         answer = network.predict(example[0])
-	error += math.fabs(1-(answer/example[1]))
+	error += math.fabs(1-math.fabs((answer/example[1])))
 	print "Prediction = ",str(answer) +";", "Target = ", example[1]
     print "Average prediction accuracy = %.2f%%" %((1-error/total) *100)
     return error / total
 
+# returns random x's and y's
 def make_x_y(start_x, range_x, start_y, range_y):
      x = random.randint(start_x ,range_x)/200.0
      y = random.randint(start_y ,range_y)/200.0
@@ -39,6 +41,8 @@ def main():
     
     examples = []
     new_examples=[]
+    
+    # create examples
     if sys.argv[1] in dummy_runs:
         if sys.argv[1] == "--dummy_xor":
             examples = new_examples = [((1, 0, 0), 0),
@@ -86,8 +90,8 @@ def main():
 
         if sys.argv[1] == "--dummy_subtract":
             for i in range(100):
-                (x, y) = make_x_y(50, 100, 0, 50)
-                (a, b) = make_x_y(50, 100, 0, 50)
+                (x, y) = make_x_y(100, 200, 0, 100)
+                (a, b) = make_x_y(100, 200, 0, 100)
                 z = x - y
                 c = a - b
                 examples.append(((1, x, y), z))
@@ -95,8 +99,8 @@ def main():
                 
         if sys.argv[1] == "--dummy_divide":
             for i in range(100):
-                (x, y) = make_x_y(1, 50, 50, 100)
-                (a, b) = make_x_y(1, 50, 50, 100)
+                (x, y) = make_x_y(1, 100, 100, 200)
+                (a, b) = make_x_y(1, 100, 100, 200)
                 c = a / b
                 z = x / y
                 examples.append(((1, x, y), z))
@@ -104,8 +108,8 @@ def main():
 
         if sys.argv[1] == "--dummy_multiply":
             for i in range(100):
-                (x, y) = make_x_y(1, 100, 1, 100)
-                (a, b) = make_x_y(1, 100, 1, 100)
+                (x, y) = make_x_y(20, 200, 50, 200)
+                (a, b) = make_x_y(20, 200, 50, 200)
                 c = a * b
                 z = y * x
                 examples.append(((1, x, y), z))
@@ -115,9 +119,13 @@ def main():
 
         net = NeuralNetwork([5, 5, 2, 7], examples)
         net.train()
+
+	# test logic operators on self.
 	if sys.argv[1] in dummy_runs[:6]:
             acc = net.test_on_self()
-	    print "Accuracy on the logic operator was: %.2f%%" %((acc) *100) 
+	    print "Accuracy on the logic operator", str(sys.argv[1])[8:], "was: %.2f%%" %((acc) *100) 
+	    
+	# test math functions on new examples.
         else:        
             acc = test_on_new_examples(net, new_examples)
     
